ce3d757b61a380ccc0343f181df7c1558a671865
choiho870712 <45503529+choiho870712@users.noreply.github.com>
2019-01-19 13:50:27 +0800

    raw data transfter to csv

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,79 +1,71 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[8]:
 
 
 import json
 import csv
 
 
-# In[49]:
+# In[10]:
 
 
 def read_json(filename):
     return json.loads(open(filename, 'r', encoding='UTF-8').read())
+
+
+# In[17]:
+
 
 def write_csv(data, filename):
     with open(filename, 'w', newline='') as csvfile:
         # 建立 CSV 檔寫入器
         writer = csv.writer(csvfile)
         # 寫入第一列資料
-        writer.writerow(['station', 
-                         'time', 
+        writer.writerow(['time', 
                          'air_pressure', 
                          'temperature', 
                          'humidity', 
                          'wind_speed', 
                          'wind_direction', 
                          'rainfall',
-                         'sunshine time'])
-        # 寫入剩餘資料
-        for every_station in data['cwbopendata']['dataset']['location'] :
-            station_name = every_station['locationName']
-            for every_time in every_station['weatherElement'][0]['time'] :
-                if ( every_time['weatherElement'][5]['elementValue']['value'] == 'T' ) :
-                    rainfall = 0.05
-                else :
-                    rainfall = every_time['weatherElement'][5]['elementValue']['value']
-                try :
-                    writer.writerow([station_name, 
-                                     every_time['obsTime'], 
-                                     every_time['weatherElement'][0]['elementValue']['value'], 
-                                     every_time['weatherElement'][1]['elementValue']['value'], 
-                                     every_time['weatherElement'][2]['elementValue']['value'], 
-                                     every_time['weatherElement'][3]['elementValue']['value'], 
-                                     every_time['weatherElement'][4]['elementValue']['value'], 
-                                     rainfall,
-                                     every_time['weatherElement'][6]['elementValue']['value']])
-                except :
-                    writer.writerow([station_name, 
-                                     every_time['obsTime'], 
-                                     every_time['weatherElement'][0]['elementValue']['value'], 
-                                     every_time['weatherElement'][1]['elementValue']['value'], 
-                                     every_time['weatherElement'][2]['elementValue']['value'], 
-                                     every_time['weatherElement'][3]['elementValue']['value'], 
-                                     every_time['weatherElement'][4]['elementValue']['value'], 
-                                     rainfall,
-                                     0.0])
+                         'sunshine_time'])
+        # 寫入資料
+        for every_time in data['cwbopendata']['dataset']['location'][0]['weatherElement'][0]['time'] :     
+            # 若夜晚 ,日照 = 0
+            try :
+                sunshine_time = every_time['weatherElement'][6]['elementValue']['value']
+            except :
+                sunshine_time = 0
+            
+            # 寫入這一行
+            writer.writerow([every_time['obsTime'], 
+                             every_time['weatherElement'][0]['elementValue']['value'], 
+                             every_time['weatherElement'][1]['elementValue']['value'], 
+                             every_time['weatherElement'][2]['elementValue']['value'], 
+                             every_time['weatherElement'][3]['elementValue']['value'], 
+                             every_time['weatherElement'][4]['elementValue']['value'], 
+                             every_time['weatherElement'][5]['elementValue']['value'],
+                             sunshine_time])
 
 
-# In[5]:
+# In[12]:
 
 
 data = read_json("C-B0024-002.json")
 
 
-# In[7]:
+# In[13]:
 
 
 data
 
 
-# In[51]:
+# In[19]:
 
 
-write_csv(data,"data.csv")
+write_csv(data,"banqiao.csv")
 
 
 # In[ ]:
